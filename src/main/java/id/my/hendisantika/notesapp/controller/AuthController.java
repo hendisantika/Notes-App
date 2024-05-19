a328a02e758e68b152b51036fa7d0e5d3614fb73
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 06:57:21 +0700

    Add registerUser in AuthController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/notesapp/controller/AuthController.java
+++ src/main/java/id/my/hendisantika/notesapp/controller/AuthController.java
@@ -1,7 +1,12 @@
 package id.my.hendisantika.notesapp.controller;
 
+import id.my.hendisantika.notesapp.model.ERole;
+import id.my.hendisantika.notesapp.model.Role;
+import id.my.hendisantika.notesapp.model.User;
 import id.my.hendisantika.notesapp.payload.request.LoginRequest;
+import id.my.hendisantika.notesapp.payload.request.SignupRequest;
 import id.my.hendisantika.notesapp.payload.response.JwtResponse;
+import id.my.hendisantika.notesapp.payload.response.MessageResponse;
 import id.my.hendisantika.notesapp.repository.RoleRepository;
 import id.my.hendisantika.notesapp.repository.UserRepository;
 import id.my.hendisantika.notesapp.security.UserDetailsImpl;
@@ -21,7 +26,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -68,5 +75,60 @@ public class AuthController {
                 userDetails.getUsername(),
                 userDetails.getEmail(),
                 roles));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "mod":
+                        Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(modRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
     }
 }
--- src/main/java/id/my/hendisantika/notesapp/model/User.java
+++ src/main/java/id/my/hendisantika/notesapp/model/User.java
@@ -62,4 +62,10 @@ public class User {
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 }
