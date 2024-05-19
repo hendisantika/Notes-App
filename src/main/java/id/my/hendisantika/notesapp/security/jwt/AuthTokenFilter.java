bdccbc77652f9532bf773f846f3c4e4936a7fe17
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 06:46:50 +0700

    Add parseJwt in AuthTokenFilter🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/notesapp/security/jwt/AuthTokenFilter.java
+++ src/main/java/id/my/hendisantika/notesapp/security/jwt/AuthTokenFilter.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -56,4 +57,13 @@ public class AuthTokenFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7);
+        }
+
+        return null;
+    }
 }
