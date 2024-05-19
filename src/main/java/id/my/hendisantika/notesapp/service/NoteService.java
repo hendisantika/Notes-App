c9bd70e9f71b36abf6160fab81206945fbc9c8a3
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 06:42:03 +0700

    Add getNotes in NoteService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/notesapp/service/NoteService.java
+++ src/main/java/id/my/hendisantika/notesapp/service/NoteService.java
@@ -1,8 +1,13 @@
 package id.my.hendisantika.notesapp.service;
 
+import id.my.hendisantika.notesapp.model.Note;
 import id.my.hendisantika.notesapp.repository.NoteRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +23,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class NoteService {
     private final NoteRepository noteRepository;
+
+    public ResponseEntity<?> getNotes(Long userId) {
+        Optional<List<Note>> notes = noteRepository.findByUserId(userId);
+        if (notes.isEmpty())
+            return ResponseEntity.badRequest().body("Error: No notes found");
+        return ResponseEntity.ok(notes);
+    }
 }
