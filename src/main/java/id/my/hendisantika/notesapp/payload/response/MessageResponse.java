00b511df2e176e093d3684fbef7d920a7fe99fce
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 06:54:50 +0700

    Add MessageResponse response🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/notesapp/payload/response/MessageResponse.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.notesapp.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Notes-App
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/20/24
+ * Time: 06:54
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageResponse {
+    private String message;
+}
