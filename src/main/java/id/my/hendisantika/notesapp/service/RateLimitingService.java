0baddf160f063a6e2c808bde31b11904c800a939
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 06:44:27 +0700

    Add allowRequest in RateLimitingService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/notesapp/service/RateLimitingService.java
+++ src/main/java/id/my/hendisantika/notesapp/service/RateLimitingService.java
@@ -24,4 +24,32 @@ public class RateLimitingService {
     private int REQUEST_LIMIT;  // Change as needed
     @Value("${app.requestLimitMS}")
     private long TIME_INTERVAL_MILLIS;  // Change as needed (1 minute in this example)
+
+    public boolean allowRequest(String clientId) {
+        long currentTime = System.currentTimeMillis();
+
+        clientRequestCounts.putIfAbsent(clientId, 1);
+
+        // Check if client is in the map
+        if (clientRequestTimestamps.containsKey(clientId)) {
+            // Get the last request timestamp for the client
+            long lastRequestTime = clientRequestTimestamps.get(clientId);
+
+            // Check if the time elapsed since the last request is within the limit
+            if (currentTime - lastRequestTime < TIME_INTERVAL_MILLIS) {
+                int requestCount = clientRequestCounts.get(clientId);
+                if (requestCount >= REQUEST_LIMIT) {
+                    // Limit exceeded, deny request
+                    return false;
+                }
+                clientRequestCounts.put(clientId, clientRequestCounts.get(clientId) + 1);
+            } else {
+                clientRequestCounts.remove(clientId);
+                clientRequestCounts.putIfAbsent(clientId, 1);
+            }
+        }
+
+        clientRequestTimestamps.put(clientId, currentTime);
+        return true;
+    }
 }
