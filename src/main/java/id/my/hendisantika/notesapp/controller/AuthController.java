83df5e814568aa06d1313e4235173efef7fe3eed
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 06:55:39 +0700

    Add AuthController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/notesapp/controller/AuthController.java
@@ -0,0 +1,38 @@
+package id.my.hendisantika.notesapp.controller;
+
+import id.my.hendisantika.notesapp.repository.RoleRepository;
+import id.my.hendisantika.notesapp.repository.UserRepository;
+import id.my.hendisantika.notesapp.security.jwt.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Notes-App
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/20/24
+ * Time: 06:55
+ * To change this template use File | Settings | File Templates.
+ */
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+
+    private final UserRepository userRepository;
+
+    private final RoleRepository roleRepository;
+
+    private final PasswordEncoder encoder;
+
+    private final JwtUtils jwtUtils;
+
+}
