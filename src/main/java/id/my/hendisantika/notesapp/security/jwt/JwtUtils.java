aa3dc76810780faab8e4c02faeba3152b9e2ec41
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 06:48:25 +0700

    Add getUserNameFromJwtToken in JwtUtils🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/notesapp/security/jwt/JwtUtils.java
+++ src/main/java/id/my/hendisantika/notesapp/security/jwt/JwtUtils.java
@@ -45,4 +45,9 @@ public class JwtUtils {
     private Key key() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key()).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
 }
