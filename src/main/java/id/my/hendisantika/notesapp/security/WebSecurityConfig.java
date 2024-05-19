afb0de6b74f57aab62f8c6e15f68b5a266af49a1
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 06:39:58 +0700

    Add WebSecurityConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/notesapp/security/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.notesapp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Notes-App
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/20/24
+ * Time: 06:39
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableMethodSecurity
+// (securedEnabled = true,
+// jsr250Enabled = true,
+// prePostEnabled = true) // by default
+public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+}
