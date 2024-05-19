a09d149c2b2e4689abc8f1823cc1babf2e308cdc
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 06:37:22 +0700

    Add Role Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/notesapp/model/Role.java
@@ -0,0 +1,38 @@
+package id.my.hendisantika.notesapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Notes-App
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/20/24
+ * Time: 06:36
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "roles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+}
