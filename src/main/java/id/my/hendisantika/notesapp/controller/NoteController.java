93da57b8d04a58e0d3ef8424a385e2eafe08c179
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 06:58:37 +0700

    Add saveNote in NoteController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/notesapp/controller/NoteController.java
+++ src/main/java/id/my/hendisantika/notesapp/controller/NoteController.java
@@ -1,5 +1,7 @@
 package id.my.hendisantika.notesapp.controller;
 
+import id.my.hendisantika.notesapp.model.Note;
+import id.my.hendisantika.notesapp.payload.request.NoteRequest;
 import id.my.hendisantika.notesapp.security.UserDetailsImpl;
 import id.my.hendisantika.notesapp.service.NoteService;
 import id.my.hendisantika.notesapp.service.RateLimitingService;
@@ -10,6 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,6 +50,24 @@ public class NoteController {
         Long userId = ((UserDetailsImpl) authentication.getPrincipal()).getId();
         if (rateLimitingService.allowRequest(userId.toString()))
             return noteService.getNoteByNoteId(userId, noteId);
+        return ResponseEntity.status(429).body("Request limit exceeded");
+    }
+
+    @PostMapping("/")
+    ResponseEntity<?> saveNote(@RequestBody NoteRequest noteRequest, Authentication authentication) {
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        Long userId = userDetails.getId();
+
+        if (rateLimitingService.allowRequest(userId.toString())) {
+            if (userId != null) {
+                Note note = new Note();
+                note.setUserId(userId);
+                note.setTitle(noteRequest.getTitle());
+                note.setContent(noteRequest.getContent());
+                return noteService.createNote(note);
+            }
+            return ResponseEntity.badRequest().body("Authentication Failed");
+        }
         return ResponseEntity.status(429).body("Request limit exceeded");
     }
 }
