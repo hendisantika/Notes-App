356d76901837d5df436ac90619aae7d75e9a1585
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 06:40:26 +0700

    Add authenticationProvider in WebSecurityConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/notesapp/security/WebSecurityConfig.java
+++ src/main/java/id/my/hendisantika/notesapp/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.notesapp.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 /**
@@ -31,4 +32,21 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
     public AuthTokenFilter authenticationJwtTokenFilter() {
         return new AuthTokenFilter();
     }
+
+    //  @Override
+//  public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//    authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+//  }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+//  @Bean
 }
