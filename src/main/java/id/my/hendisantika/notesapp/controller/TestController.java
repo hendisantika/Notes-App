6f407b22823cbd83fc8e34913f35b7dfe785920e
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 06:59:47 +0700

    Add TestController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/notesapp/controller/TestController.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.notesapp.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Notes-App
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/20/24
+ * Time: 06:59
+ * To change this template use File | Settings | File Templates.
+ */
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+}
