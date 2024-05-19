3d6009880eb2487c93ad4fc423d73066163ecdf7
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 06:36:11 +0700

    Add ERole Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/notesapp/model/ERole.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.notesapp.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Notes-App
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/20/24
+ * Time: 06:35
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
