db72741cd6e94ceb2609d1830cccf57dae6f4cb7
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 07:32:04 +0700

    Bug Fix Test Container🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/notesapp/HendiPostgresqlContainer.java
@@ -0,0 +1,38 @@
+package id.my.hendisantika.notesapp;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Notes-App
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/20/24
+ * Time: 07:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class HendiPostgresqlContainer extends PostgreSQLContainer<HendiPostgresqlContainer> {
+    private static final String IMAGE_VERSION = "postgres:16";
+    private static HendiPostgresqlContainer container;
+
+    private HendiPostgresqlContainer() {
+        super(IMAGE_VERSION);
+    }
+
+    public static HendiPostgresqlContainer getInstance() {
+        if (container == null) {
+            container = new HendiPostgresqlContainer();
+        }
+        return container;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        System.setProperty("DB_URL", container.getJdbcUrl());
+        System.setProperty("DB_USERNAME", container.getUsername());
+        System.setProperty("DB_PASSWORD", container.getPassword());
+    }
+
+}
--- src/test/java/id/my/hendisantika/notesapp/NotesAppApplicationTests.java
+++ src/test/java/id/my/hendisantika/notesapp/NotesAppApplicationTests.java
@@ -1,10 +1,39 @@
 package id.my.hendisantika.notesapp;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class NotesAppApplicationTests {
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    );
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Test
     void contextLoads() {
